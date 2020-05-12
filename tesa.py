1daa9de840d44ff33c77c6e217a9541a78761e69
manbek1978 <60145799+manbek1978@users.noreply.github.com>
2020-05-12 13:33:14 +0500

    Add files via upload

---
+++ tesa.py
@@ -0,0 +1,513 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'tesa.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(738, 579)
+        Dialog.setStyleSheet("background-color: #F1FFC8;")
+        self.gridLayoutWidget = QtWidgets.QWidget(Dialog)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(20, 230, 691, 344))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        self.pushButton_24 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_24.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #59E91B;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}\n"
+"")
+        self.pushButton_24.setObjectName("pushButton_24")
+        self.gridLayout.addWidget(self.pushButton_24, 9, 2, 1, 1)
+        self.pushButton_16 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_16.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #59E91B;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}\n"
+"")
+        self.pushButton_16.setObjectName("pushButton_16")
+        self.gridLayout.addWidget(self.pushButton_16, 5, 2, 1, 1)
+        self.pushButton_28 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_28.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #59E91B;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}\n"
+"")
+        self.pushButton_28.setObjectName("pushButton_28")
+        self.gridLayout.addWidget(self.pushButton_28, 10, 2, 1, 1)
+        self.pushButton_7 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_7.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #B34EE9 ;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}")
+        self.pushButton_7.setObjectName("pushButton_7")
+        self.gridLayout.addWidget(self.pushButton_7, 4, 0, 1, 1)
+        self.pushButton_14 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_14.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #B34EE9 ;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}")
+        self.pushButton_14.setObjectName("pushButton_14")
+        self.gridLayout.addWidget(self.pushButton_14, 8, 0, 1, 1)
+        self.pushButton_29 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_29.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #3737E9;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}")
+        self.pushButton_29.setObjectName("pushButton_29")
+        self.gridLayout.addWidget(self.pushButton_29, 11, 1, 1, 1)
+        self.pushButton_30 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_30.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #59E91B;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}\n"
+"")
+        self.pushButton_30.setObjectName("pushButton_30")
+        self.gridLayout.addWidget(self.pushButton_30, 11, 2, 1, 1)
+        self.pushButton_26 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_26.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #B34EE9 ;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}")
+        self.pushButton_26.setObjectName("pushButton_26")
+        self.gridLayout.addWidget(self.pushButton_26, 10, 0, 1, 1)
+        self.pushButton_11 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_11.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #B34EE9 ;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}")
+        self.pushButton_11.setObjectName("pushButton_11")
+        self.gridLayout.addWidget(self.pushButton_11, 5, 0, 1, 1)
+        self.pushButton_23 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_23.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #3737E9;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}")
+        self.pushButton_23.setObjectName("pushButton_23")
+        self.gridLayout.addWidget(self.pushButton_23, 9, 1, 1, 1)
+        self.pushButton_19 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_19.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #B34EE9 ;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}")
+        self.pushButton_19.setObjectName("pushButton_19")
+        self.gridLayout.addWidget(self.pushButton_19, 7, 0, 1, 1)
+        self.pushButton_18 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_18.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #59E91B;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}\n"
+"")
+        self.pushButton_18.setObjectName("pushButton_18")
+        self.gridLayout.addWidget(self.pushButton_18, 8, 2, 1, 1)
+        self.pushButton_13 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_13.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #B34EE9 ;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}")
+        self.pushButton_13.setObjectName("pushButton_13")
+        self.gridLayout.addWidget(self.pushButton_13, 6, 0, 1, 1)
+        self.pushButton_20 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_20.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #3737E9;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}")
+        self.pushButton_20.setObjectName("pushButton_20")
+        self.gridLayout.addWidget(self.pushButton_20, 7, 1, 1, 1)
+        self.pushButton_21 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_21.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #59E91B;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}\n"
+"")
+        self.pushButton_21.setObjectName("pushButton_21")
+        self.gridLayout.addWidget(self.pushButton_21, 7, 2, 1, 1)
+        self.pushButton_15 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_15.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #3737E9;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}")
+        self.pushButton_15.setObjectName("pushButton_15")
+        self.gridLayout.addWidget(self.pushButton_15, 6, 1, 1, 1)
+        self.pushButton_17 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_17.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #59E91B;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}\n"
+"")
+        self.pushButton_17.setObjectName("pushButton_17")
+        self.gridLayout.addWidget(self.pushButton_17, 6, 2, 1, 1)
+        self.pushButton_8 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_8.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #3737E9;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}")
+        self.pushButton_8.setObjectName("pushButton_8")
+        self.gridLayout.addWidget(self.pushButton_8, 4, 1, 1, 1)
+        self.pushButton_2 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_2.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #3737E9;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}")
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.gridLayout.addWidget(self.pushButton_2, 0, 1, 1, 1)
+        self.pushButton_27 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_27.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #3737E9;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}")
+        self.pushButton_27.setObjectName("pushButton_27")
+        self.gridLayout.addWidget(self.pushButton_27, 10, 1, 1, 1)
+        self.pushButton_10 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_10.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #59E91B;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}\n"
+"")
+        self.pushButton_10.setObjectName("pushButton_10")
+        self.gridLayout.addWidget(self.pushButton_10, 3, 2, 1, 1)
+        self.pushButton_22 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_22.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #B34EE9 ;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}")
+        self.pushButton_22.setObjectName("pushButton_22")
+        self.gridLayout.addWidget(self.pushButton_22, 9, 0, 1, 1)
+        self.pushButton_3 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_3.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #B34EE9 ;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}")
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.gridLayout.addWidget(self.pushButton_3, 0, 0, 1, 1)
+        self.pushButton = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #59E91B;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}\n"
+"")
+        self.pushButton.setObjectName("pushButton")
+        self.gridLayout.addWidget(self.pushButton, 0, 2, 1, 1)
+        self.pushButton_12 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_12.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #3737E9;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}")
+        self.pushButton_12.setObjectName("pushButton_12")
+        self.gridLayout.addWidget(self.pushButton_12, 8, 1, 1, 1)
+        self.pushButton_25 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_25.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #B34EE9 ;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}")
+        self.pushButton_25.setObjectName("pushButton_25")
+        self.gridLayout.addWidget(self.pushButton_25, 11, 0, 1, 1)
+        self.pushButton_9 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_9.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #3737E9;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}")
+        self.pushButton_9.setObjectName("pushButton_9")
+        self.gridLayout.addWidget(self.pushButton_9, 5, 1, 1, 1)
+        self.pushButton_6 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_6.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #B34EE9 ;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}")
+        self.pushButton_6.setObjectName("pushButton_6")
+        self.gridLayout.addWidget(self.pushButton_6, 1, 0, 1, 1)
+        self.pushButton_5 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_5.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #3737E9;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}")
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.gridLayout.addWidget(self.pushButton_5, 1, 1, 1, 1)
+        self.pushButton_4 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_4.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #59E91B;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}\n"
+"")
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.gridLayout.addWidget(self.pushButton_4, 1, 2, 1, 1)
+        self.pushButton_31 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_31.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #B34EE9 ;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}")
+        self.pushButton_31.setObjectName("pushButton_31")
+        self.gridLayout.addWidget(self.pushButton_31, 3, 0, 1, 1)
+        self.pushButton_32 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_32.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #3737E9;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}")
+        self.pushButton_32.setObjectName("pushButton_32")
+        self.gridLayout.addWidget(self.pushButton_32, 3, 1, 1, 1)
+        self.pushButton_33 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_33.setStyleSheet("QPushButton{\n"
+"font: ;\n"
+"background-color: #59E91B;\n"
+"color: #080A12;\n"
+"}\n"
+"QPushButton:hover{\n"
+"font: ;\n"
+"background-color: red;\n"
+"\n"
+"}\n"
+"")
+        self.pushButton_33.setObjectName("pushButton_33")
+        self.gridLayout.addWidget(self.pushButton_33, 4, 2, 1, 1)
+        self.textEdit = QtWidgets.QTextEdit(Dialog)
+        self.textEdit.setGeometry(QtCore.QRect(20, 10, 691, 211))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.textEdit.setFont(font)
+        self.textEdit.setStyleSheet("background-color: #00FFA9;")
+        self.textEdit.setObjectName("textEdit")
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.pushButton_24.setText(_translate("Dialog", "Ustozlar"))
+        self.pushButton_16.setText(_translate("Dialog", "Ustozlar"))
+        self.pushButton_28.setText(_translate("Dialog", "Tez yordam"))
+        self.pushButton_7.setText(_translate("Dialog", "4 \'A\' sinf"))
+        self.pushButton_14.setText(_translate("Dialog", "8 \'A\' sinf"))
+        self.pushButton_29.setText(_translate("Dialog", "Rayono"))
+        self.pushButton_30.setText(_translate("Dialog", "Maktab directori"))
+        self.pushButton_26.setText(_translate("Dialog", "Yoshlar ittifoki "))
+        self.pushButton_11.setText(_translate("Dialog", "5 \'A\' sinf"))
+        self.pushButton_23.setText(_translate("Dialog", "9 \'B\' sinf"))
+        self.pushButton_19.setText(_translate("Dialog", "7 \'A\' sinf"))
+        self.pushButton_18.setText(_translate("Dialog", "Ustozlar"))
+        self.pushButton_13.setText(_translate("Dialog", "6 \'A\' sinf"))
+        self.pushButton_20.setText(_translate("Dialog", "7 \'B\' sinf"))
+        self.pushButton_21.setText(_translate("Dialog", "Ustozlar"))
+        self.pushButton_15.setText(_translate("Dialog", "6 \'B\' sinf"))
+        self.pushButton_17.setText(_translate("Dialog", "Ustozlar"))
+        self.pushButton_8.setText(_translate("Dialog", "4 \'B\' sinf"))
+        self.pushButton_2.setText(_translate("Dialog", "1 \'b\' sinf  "))
+        self.pushButton_27.setText(_translate("Dialog", "Ota onalar kengashi"))
+        self.pushButton_10.setText(_translate("Dialog", "Ustozlar"))
+        self.pushButton_22.setText(_translate("Dialog", "9 \'A\' sinf"))
+        self.pushButton_3.setText(_translate("Dialog", "1 \'A\' sinf"))
+        self.pushButton.setText(_translate("Dialog", "Ustozlar"))
+        self.pushButton_12.setText(_translate("Dialog", "8 \'B\' sinf"))
+        self.pushButton_25.setText(_translate("Dialog", "Ishonch raqamlari"))
+        self.pushButton_9.setText(_translate("Dialog", "5 \'B\' sinf"))
+        self.pushButton_6.setText(_translate("Dialog", "2 \'A\' sinf"))
+        self.pushButton_5.setText(_translate("Dialog", "2 \'B\' sinf"))
+        self.pushButton_4.setText(_translate("Dialog", "Ustozlar"))
+        self.pushButton_31.setText(_translate("Dialog", "3 \'A\' sinf"))
+        self.pushButton_32.setText(_translate("Dialog", "3 \'B\' sinf"))
+        self.pushButton_33.setText(_translate("Dialog", "Ustozlar"))
+
+
+
